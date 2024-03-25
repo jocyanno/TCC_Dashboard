a57b5c395f9a4f1aee51fa0cb1c593b20f5cdab0
jocyanno <jocyannovittor@hotmail.com>
2024-03-25 17:18:28 -0300

    fix: inserido select para datas no gráficos de linhas tela 2 e 3 e adicionado tratamento de erros

--- src/page2/seriesTemporais.py
+++ src/page2/seriesTemporais.py
@@ -2,40 +2,57 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import streamlit as st
-from src.utils.tamanho_tela import tamanho_tela
 
 def series_temporais(widthImage):
-  # Suponha que temos o seguinte DataFrame
-  data = pd.DataFrame(
-    {
-      "data": pd.date_range(start="1/1/2022", periods=100),
-      "Piedade": np.random.randint(1, 20, 100),
-      "Socorro": np.random.randint(1, 20, 100),
-      "Muribeca": np.random.randint(1, 20, 100),
-    }
-  )
+  try:
+    # Suponha que temos o seguinte DataFrame
+    data = pd.DataFrame(
+      {
+        "data": pd.date_range(start="1/1/2022", periods=100),
+        "Piedade": np.random.randint(1, 20, 100),
+        "Socorro": np.random.randint(1, 20, 100),
+        "Muribeca": np.random.randint(1, 20, 100),
+      }
+    )
 
-  # Selecionando a região
-  regiao = st.selectbox(
-    "Selecione a região", options=["Todas", "Piedade", "Socorro", "Muribeca"]
-  )
+    # Selecionando a região
+    regiao = st.selectbox(
+      "Selecione a região", options=["Todas", "Piedade", "Socorro", "Muribeca"]
+    )
+    
+    # Seletor de tempo
+    tempo = st.date_input(
+        "Selecione o intervalo de tempo", [data['data'].min().date(), data['data'].max().date()]
+    )
 
-  if regiao == "Todas":
-    for reg in data.columns[1:]:
-      # Criando o gráfico de séries temporais para cada região
-      fig = px.line(data, x="data", y=reg)
+    # Converte o tempo diretamente em objetos de data
+    inicio = pd.to_datetime(tempo[0])
+    fim = pd.to_datetime(tempo[1])
+
+  
+    mask = (data['data'] >= inicio) & (data['data'] <= fim)
+    data_filtrada = data.loc[mask]
+  
+    if regiao == "Todas":
+      for reg in data_filtrada.columns[1:]:
+        # Criando o gráfico de séries temporais para cada região
+        fig = px.line(data_filtrada, x="data", y=reg)
+        # Adicionando a legenda ao eixo y
+        fig.update_yaxes(title_text="Milimetros de chuva (mm)")
+        fig.update_layout(width=widthImage)
+        # Exibindo o título do gráfico
+        st.header(reg)
+        st.plotly_chart(fig)
+    else:
+      # Criando o gráfico de séries temporais para a região selecionada
+      fig = px.line(data_filtrada, x="data", y=regiao)
+      st.markdown(f"### {regiao}")
       # Adicionando a legenda ao eixo y
       fig.update_yaxes(title_text="Milimetros de chuva (mm)")
       fig.update_layout(width=widthImage)
-      # Exibindo o título do gráfico
-      st.header(reg)
+      # Exibindo o gráfico
       st.plotly_chart(fig)
-  else:
-    # Criando o gráfico de séries temporais para a região selecionada
-    fig = px.line(data, x="data", y=regiao)
-    st.markdown(f"### {regiao}")
-    # Adicionando a legenda ao eixo y
-    fig.update_yaxes(title_text="Milimetros de chuva (mm)")
-    fig.update_layout(width=widthImage)
-    # Exibindo o gráfico
-    st.plotly_chart(fig)
+  
+  except ValueError as e:
+      st.error("Não foram encontrados dados para a data selecionada.")
+      return
--- src/page3/grafico.py
+++ src/page3/grafico.py
@@ -1,3 +1,4 @@
+import datetime
 import plotly.graph_objects as px
 import numpy as np
 import streamlit as st
@@ -21,65 +22,97 @@ def graficoAlagamentoseDeslizamentos(widthImage):
     datas = pd.date_range(start="1/1/2020", periods=100).to_pydatetime().tolist()
     datas.sort()
 
+    # Seletor de tempo
+    tempo = st.date_input(
+        "Selecione o intervalo de tempo", [datas[0].date(), datetime.date(2020, 12, 31)]
+    )
+
+    # Converte o tempo diretamente em objetos de data
+    inicio = pd.to_datetime(tempo[0])
+    fim = pd.to_datetime(tempo[1])
+
+    try:
+        mask = (np.array(datas) >= inicio) & (np.array(datas) <= fim)
+        datas_filtradas = np.array(datas)[mask]
+    except ValueError as e:
+        st.error("Não foram encontrados dados para a data selecionada.")
+        return
+
     if regiao == "Todas":
-      for reg, (chuva_mm, deslizamentos) in dados.items():
-        plot = go.Figure()
+        for reg, (chuva_mm, deslizamentos) in dados.items():
+            plot_chuva = go.Figure()
 
-        plot.add_trace(
-          go.Scatter(
-            x=datas,
-            y=chuva_mm,
-            mode="lines",
-            name="Chuva em mm",
-            hovertemplate="<b>Data:</b> %{x}<br>"
-            + "<b>Chuva (mm):</b> %{y}<br>",
-          )
-        )
+            plot_chuva.add_trace(
+                go.Scatter(
+                    x=datas_filtradas,
+                    y=chuva_mm,
+                    mode="lines",
+                    name="Chuva em mm",
+                    hovertemplate="<b>Data:</b> %{x}<br>"
+                    + "<b>Chuva (mm):</b> %{y}<br>",
+                )
+            )
 
-        plot.add_trace(
-          go.Scatter(
-            x=datas,
-            y=deslizamentos,
-            mode="lines",
-            name="Deslizamentos",
-            hovertemplate="<b>Data:</b> %{x}<br>"
-            + "<b>Deslizamentos:</b> %{y}<br>",
-          )
-        )
+            plot_chuva.update_xaxes(title_text="Data")
+            plot_chuva.update_yaxes(title_text="Chuva em mm")
+            plot_chuva.update_layout(width=widthImage, template="plotly_white")
+            st.header(f"Chuva em mm - {reg}")
+            st.plotly_chart(plot_chuva)
 
-        plot.update_xaxes(title_text="Data")
-        plot.update_yaxes(title_text="Quantidade")
-        plot.update_layout(width=widthImage, template='plotly_white')
-        st.header(reg)
-        st.plotly_chart(plot)
+            plot_deslizamentos = go.Figure()
+
+            plot_deslizamentos.add_trace(
+                go.Scatter(
+                    x=datas_filtradas,
+                    y=deslizamentos,
+                    mode="lines",
+                    name="Deslizamentos",
+                    hovertemplate="<b>Data:</b> %{x}<br>"
+                    + "<b>Deslizamentos:</b> %{y}<br>",
+                )
+            )
+
+            plot_deslizamentos.update_xaxes(title_text="Data")
+            plot_deslizamentos.update_yaxes(title_text="Deslizamentos")
+            plot_deslizamentos.update_layout(width=widthImage, template="plotly_white")
+            st.header(f"Deslizamentos - {reg}")
+            st.plotly_chart(plot_deslizamentos)
     else:
-      chuva_mm, deslizamentos = dados[regiao]
-      plot = go.Figure()
+        chuva_mm, deslizamentos = dados[regiao]
 
-      plot.add_trace(
-        go.Scatter(
-          x=datas,
-          y=chuva_mm,
-          mode="lines",
-          name="Chuva em mm",
-          hovertemplate="<b>Data:</b> %{x}<br>"
-          + "<b>Chuva (mm):</b> %{y}<br>",
+        plot_chuva = go.Figure()
+
+        plot_chuva.add_trace(
+            go.Scatter(
+                x=datas_filtradas,
+                y=chuva_mm,
+                mode="lines",
+                name="Chuva em mm",
+                hovertemplate="<b>Data:</b> %{x}<br>" + "<b>Chuva (mm):</b> %{y}<br>",
+            )
         )
-      )
 
-      plot.add_trace(
-        go.Scatter(
-          x=datas,
-          y=deslizamentos,
-          mode="lines",
-          name="Deslizamentos",
-          hovertemplate="<b>Data:</b> %{x}<br>"
-          + "<b>Deslizamentos:</b> %{y}<br>",
+        plot_chuva.update_xaxes(title_text="Data")
+        plot_chuva.update_yaxes(title_text="Chuva em mm")
+        plot_chuva.update_layout(width=widthImage, template="plotly_white")
+        st.header(f"Chuva em mm - {regiao}")
+        st.plotly_chart(plot_chuva)
+
+        plot_deslizamentos = go.Figure()
+
+        plot_deslizamentos.add_trace(
+            go.Scatter(
+                x=datas_filtradas,
+                y=deslizamentos,
+                mode="lines",
+                name="Deslizamentos",
+                hovertemplate="<b>Data:</b> %{x}<br>"
+                + "<b>Deslizamentos:</b> %{y}<br>",
+            )
         )
-      )
 
-      plot.update_xaxes(title_text="Data")
-      plot.update_yaxes(title_text="Quantidade")
-      plot.update_layout(width=widthImage, template='plotly_white')
-      st.header(regiao)
-      st.plotly_chart(plot)
+        plot_deslizamentos.update_xaxes(title_text="Data")
+        plot_deslizamentos.update_yaxes(title_text="Deslizamentos")
+        plot_deslizamentos.update_layout(width=widthImage, template="plotly_white")
+        st.header(f"Deslizamentos - {regiao}")
+        st.plotly_chart(plot_deslizamentos)
